e8dc5014c5c3d9ec693ac6ed657521c07bd92605
Vitor Madalosso <vitorconnected@gmail.com>
2025-08-29 01:42:07 -0400

    js added

--- direct-clients/queiroz-services-site/assets/js/scripts.js
+++ direct-clients/queiroz-services-site/assets/js/scripts.js
@@ -1,4 +1,4 @@
-/*========== SHOW MENU ==========*/
+  /*========== SHOW MENU ==========*/
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close')
@@ -10,14 +10,14 @@ if (navToggle) {
   })
 }
 
-/*---------- Menu Hidden ----------*/
+  /*---------- Menu Hidden ----------*/
 if (navClose) {
   navClose.addEventListener('click', () => {
       navMenu.classList.toggle('show-menu')
     })
 }
 
-/*========== REMOVE MENU MOBILE ==========*/
+  /*========== REMOVE MENU MOBILE ==========*/
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () => {
@@ -27,7 +27,7 @@ const linkAction = () => {
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*========== Change Background Header ==========*/
+  /*========== Change Background Header ==========*/
 const bgHeader = () => {
   const header = document.getElementById('header')
   this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header')
@@ -36,7 +36,7 @@ const bgHeader = () => {
 window.addEventListener('scroll', bgHeader)
 bgHeader()
 
-/*========== Swiper Serivces ==========*/
+  /*========== Swiper Serivces ==========*/
 const swiperServices = new Swiper('.services__swiper', {
   loop: true,
   grabCursor: true,
@@ -48,3 +48,50 @@ const swiperServices = new Swiper('.services__swiper', {
     prevEl: '.swiper-button-prev',
   }
 })
+
+  /*========== Scroll Up ==========*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+scrollUp()
+
+    /*========== Scroll Active Link ==========*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+    /*========== Scroll Reveal Animation ==========*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 1500,
+  delay: 400,
+  reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__content, .services__data, .services__swiper, .footer__container`)
+sr.reveal(`.home__images`, { origin: 'bottom', delay: 1000 })
+sr.reveal(`.about__images, .contact__img`, {origin: 'left'})
+sr.reveal(`.about__data, .contact__data`, { origin: 'right' })
+sr.reveal(`.projects__card`, {interval: 100})
+
